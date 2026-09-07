84010039246c58ddd9194173ecef9e243e2dd556
Tely-hafia <tely.hafia@gmail.com>
2026-09-07 17:18:03 +0200

    Utiliser le logo officiel sur la page contact

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_VERSION = "calypso-equipe-v15";
+const CACHE_VERSION = "calypso-equipe-v16";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const PAGE_CACHE = `${CACHE_VERSION}-pages`;
 const APP_SHELL = [
